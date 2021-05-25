b4eff8d1a63aa85861f53fb7c46ec99b79443eac
Mrvnqty <84437917+Mrvnqty@users.noreply.github.com>
2021-05-25 16:56:43 +0800

    Add files via upload

---
+++ Example 5 MouseEvent and MouseMotionListener Interface/src/main/MouseMotionDemo.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package main;
+
+/**
+ * @author Lenovo
+ *
+ */
+import java.awt.*;
+import java.awt.event.*;
+
+public class MouseMotionDemo extends Frame {
+	
+	private TextField tfMouseClickX;
+	private TextField tfMouseClickY;
+	
+	private TextField tfMousePositionX;
+	private TextField tfMousePositionY;
+	
+	public MouseMotionDemo() {
+		setLayout(new FlowLayout());
+		
+		add(new Label("X-Click: "));
+		tfMouseClickX = new TextField(10);
+		tfMouseClickX.setEditable(false);
+		add(tfMouseClickX);
+		add(new Label("Y-Click: "));
+		tfMouseClickY = new TextField(10);
+		tfMouseClickY.setEditable(false);
+		add(tfMouseClickY);
+		
+		add(new Label("X-Position"));
+		tfMousePositionX = new TextField(10);
+		tfMousePositionX.setEditable(false);
+		add(tfMousePositionX);
+		add(new Label("Y-Position"));
+		tfMousePositionY = new TextField(10);
+		tfMousePositionY.setEditable(false);
+		add(tfMousePositionY);
+		
+		setTitle("MouseMotion Demo");
+		setSize(400, 120);
+		setVisible(true);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new MouseMotionDemo();
+	}
+
+}
